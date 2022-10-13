55df71f760183e2a1862a9525cfca13447cefef5
Oleksandr Mazur <admin@devops.kiev.ua>
2022-10-13 20:32:11 +0300

    Added index.html

---
+++ yaml2html.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cat << EOF
+<html>
+<body>
+<h1>Helm repository</h1>
+<h2>Installation</h2>
+<p>
+<pre>
+helm repo add  oleksandr-mazur https://oleksandr-mazur.github.io/helm/
+
+helm repo update
+
+</pre>
+</p>
+<h2>Available versions</h2>
+<ul>
+
+
+</ul>
+</body>
+</html>
+EOF
